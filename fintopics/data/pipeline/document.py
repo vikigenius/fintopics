fb4f791baa89eb60953708a1f589fa142bfd5a25
Ruihan Wei <31101321+RienWave@users.noreply.github.com>
2020-07-13 03:01:08 -0400

    refined labelling, dealt with 'part','item' and 'f-' cases

--- fintopics/data/pipeline/document.py
+++ fintopics/data/pipeline/document.py
@@ -3,6 +3,7 @@
 from fintopics.data.pipeline import Pipeline
 from fintopics import config
 import re
+import string
 
 class DocumentPipeline(Pipeline):
     """Pipeline that cleans documents using regex."""
@@ -26,10 +27,37 @@ class DocumentPipeline(Pipeline):
             current_label = "none"
             data_stream["label"] = []
             for line in data_stream["text"]:
-                if line.strip() != "" and all(ch.isdigit() or ch.isupper() or ch == " " or ch == "\t" for ch in line):
-                    current_label = (re.sub(r'\d+', '', line)).lower()
-                    if current_label.strip() == "":
+                if line.strip() != "" and all(ch in string.punctuation or ch.isdigit() or ch.isupper() or ch == " " or ch == "\t" for ch in line):
+                    current_label = line.replace(",", " ")
+                    current_label = current_label.strip()
+
+                    # handling "ITEM" headers
+                    if current_label.startswith("ITEM"):
+                        label_ls = current_label.split(" ")
+                        if label_ls[1].endswith("."):
+                            newlb = ""
+                            for i in range(0, len(label_ls)):
+                                if i != 1:
+                                    newlb += label_ls[i] + " "
+                            current_label = newlb
+
+                        current_label = current_label.replace("ITEM", "")
+
+                    current_label = (re.sub(r'\d+', '', current_label)).lower()
+                    current_label = current_label.translate(str.maketrans('', '', string.punctuation))
+                    current_label = current_label.strip()
+                    if current_label == "":
                         current_label = "none"
+
+                    # handling "part" headers
+                    if current_label.startswith("part"):
+                        label_ls = current_label.split(" ")
+                        if len(label_ls) == 2:
+                            current_label = "none"
+                    # handle "F-" cases
+                    if current_label == "f":
+                        current_label = "none"
+
                 data_stream["label"].append(current_label)
 
         return data_stream
